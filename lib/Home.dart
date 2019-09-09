c5f4096d5acb26ca427f6a8a8d4efc5a42786101
Yazdani <yaz4dani@gmail.com>
2019-09-09 12:51:39 +0600

    Initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      appBar: new AppBar(
+        title: new Text("Image Carosol"),
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.remove_red_eye),
+              onPressed: ()=>debugPrint("Eye")
+          ),
+
+          new IconButton(
+              icon: new Icon(Icons.face),
+              onPressed: ()=>debugPrint("Face")
+          )
+
+        ],
+      ),
+
+    );
+  }
+}

f9c1345894b6dd2cae511d237d8675b0941bad0b
Yazdani <yaz4dani@gmail.com>
2019-09-09 12:52:21 +0600

    Second

--- lib/Home.dart
+++ lib/Home.dart
@@ -5,7 +5,6 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => new _HomeState();
 }
-
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
